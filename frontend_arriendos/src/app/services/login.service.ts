ce6e40e9944b21a8e465b8f99f28fcd3e0117e7b
Vladox69 <marwladiagua2001@hotmail.com>
2022-06-21 19:20:45 -0500

    Merge de backend

---
+++ frontend_arriendos/src/app/services/login.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http:HttpClient) { }
+
+  login():Observable<any>{
+    return this.http.post('','');
+  }
+
+}
