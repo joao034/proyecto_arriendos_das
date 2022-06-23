eaf8870aee01e5a6d50217fa826e9111ac3286ba
Vladox69 <marwladiagua2001@hotmail.com>
2022-06-22 21:06:52 -0500

    Api provincias

---
+++ backend_arriendos/api_arriendos/api_arriendos/api_arriendos/Controllers/ProvinciasController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Arriendos.Data;
+using Arriendos.Models;
+
+namespace api_arriendos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProvinciasController : ControllerBase
+    {
+        private readonly ArriendosContext _context;
+
+        public ProvinciasController(ArriendosContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Provincias
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Provincia>>> GetProvincias()
+        {
+            return await _context.Provincias.ToListAsync();
+        }
+
+        // GET: api/Provincias/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Provincia>> GetProvincia(int id)
+        {
+            var provincia = await _context.Provincias.FindAsync(id);
+
+            if (provincia == null)
+            {
+                return NotFound();
+            }
+
+            return provincia;
+        }
+
+        // PUT: api/Provincias/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProvincia(int id, Provincia provincia)
+        {
+            if (id != provincia.IdPro)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(provincia).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProvinciaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Provincias
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Provincia>> PostProvincia(Provincia provincia)
+        {
+            _context.Provincias.Add(provincia);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProvincia", new { id = provincia.IdPro }, provincia);
+        }
+
+        // DELETE: api/Provincias/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProvincia(int id)
+        {
+            var provincia = await _context.Provincias.FindAsync(id);
+            if (provincia == null)
+            {
+                return NotFound();
+            }
+
+            _context.Provincias.Remove(provincia);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProvinciaExists(int id)
+        {
+            return _context.Provincias.Any(e => e.IdPro == id);
+        }
+    }
+}
