7cc784c94badc56576dfdc6d271eedcb981d4b87
Vladox69 <marwladiagua2001@hotmail.com>
2022-07-08 10:34:14 -0500

    git commit -m "PAD-24 <FormGroup Login>"

--- frontend_arriendos/src/app/core/componentes/inicio-sesion/inicio-sesion.component.ts
+++ frontend_arriendos/src/app/core/componentes/inicio-sesion/inicio-sesion.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-inicio-sesion',
   templateUrl: './inicio-sesion.component.html',
-  styleUrls: ['./inicio-sesion.component.css']
+  styleUrls: ['./inicio-sesion.component.css'],
 })
 export class InicioSesionComponent implements OnInit {
+  loginForm = new FormGroup({
+    usernameUsu: new FormControl('', Validators.required),
+    passwordUsu: new FormControl('', Validators.required),
+  });
 
-  constructor() { }
+  constructor(private login:LoginService,private route:Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onLogin(form: any) {
+    this.login.loginByUser(form).subscribe(usuario=> {
+      if(usuario!=null){
+        localStorage.setItem('usuario',JSON.stringify(usuario));
+        this.route.navigate(['/']);
+      }
+    })
   }
-
 }
