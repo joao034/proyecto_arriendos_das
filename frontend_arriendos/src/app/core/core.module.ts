068c70d9cf14b607ff6d5e930886adf68e6d6b27
joao034 <84355621+joao034@users.noreply.github.com>
2022-07-09 15:55:37 -0500

    arreglando problemas de merge develop-consumo de arriendos

--- frontend_arriendos/src/app/core/core.module.ts
+++ frontend_arriendos/src/app/core/core.module.ts
@@ -1,16 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
-import { GridArriendosComponent } from './componentes/grid-arriendos/grid-arriendos.component';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
-
-@NgModule({
-  declarations: [
-    GridArriendosComponent
-  ],
-  imports: [CommonModule, MaterialModule],
-
 
 @NgModule({
   declarations: [
