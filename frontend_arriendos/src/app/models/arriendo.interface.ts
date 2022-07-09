316d11446fd43251d967867d9b4d1ab1fda39e19
Vladox69 <marwladiagua2001@hotmail.com>
2022-07-09 07:29:09 -0500

    PAD-52 <modelo de arriendo>

---
+++ frontend_arriendos/src/app/models/arriendo.interface.ts
@@ -0,0 +1,21 @@
+
+export interface ArriendoI{
+    idArr:number;
+    tipoArr:number;
+    usuPro:number;
+    numHab:number;
+    numBanos:number;
+    numPisos:number;
+    numPersonas:number;
+    ciudArr:number;
+    dirArr:string;
+    publicado:boolean;
+    superficie:number;
+    fecha:Date;
+    garage:boolean;
+    descArr:string;
+    chechArrendar:boolean;
+    precio:number;
+    amueblado:boolean;
+    mascota:boolean;
+}
