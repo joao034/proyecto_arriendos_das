7a51c0018d26c1c7290a7a16432a7a8c5b4eda63
Vladox69 <marwladiagua2001@hotmail.com>
2022-07-11 16:44:57 -0500

    PAD-55 <Api de busqueda>

--- backend_arriendos/api_arriendos/api_arriendos/api_arriendos/Controllers/ListaArriendosController.cs
+++ backend_arriendos/api_arriendos/api_arriendos/api_arriendos/Controllers/ListaArriendosController.cs
@@ -35,6 +35,19 @@ namespace api_arriendos.Controllers
             var anuncios = await _context.ListaArriendos.Where(anuncios => anuncios.UsuPro == id).ToListAsync();
             return anuncios;
         }
+
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<ListaArriendo>>> GetArriendosSearch(ListaArriendo busqueda)
+        {
+            var anuncios = await _context.ListaArriendos.Where(anuncios => anuncios.CiudArr==busqueda.CiudArr && 
+            anuncios.NomPro == busqueda.NomPro && 
+            anuncios.TipoArr == busqueda.TipoArr &&
+            anuncios.NumHab==busqueda.NumHab &&
+            anuncios.NumBanos==busqueda.NumBanos &&
+            anuncios.Mascota == busqueda.Mascota).ToListAsync();
+            return anuncios;
+        }
+
     }
 }
 
