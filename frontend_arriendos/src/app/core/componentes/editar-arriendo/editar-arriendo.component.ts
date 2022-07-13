3e9327d9845b24accf2f04dc3a127f9b26ba6bcd
joao034 <84355621+joao034@users.noreply.github.com>
2022-07-12 23:24:41 -0500

    PAD-32 <componente editar arriendo creado>

---
+++ frontend_arriendos/src/app/core/componentes/editar-arriendo/editar-arriendo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-arriendo',
+  templateUrl: './editar-arriendo.component.html',
+  styleUrls: ['./editar-arriendo.component.css']
+})
+export class EditarArriendoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- frontend_arriendos/src/app/core/core.module.ts
+++ frontend_arriendos/src/app/core/core.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { EditarArriendoComponent } from './componentes/editar-arriendo/editar-arriendo.component';
 
 @NgModule({
   declarations: [
+  
+    EditarArriendoComponent
   ],
   imports: [CommonModule, MaterialModule,MatSelectModule,MatFormFieldModule],
   exports: [],
