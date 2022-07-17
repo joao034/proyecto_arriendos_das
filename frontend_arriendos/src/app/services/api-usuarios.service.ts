f151acb2ea0450217ec6e7d66b7fc870515b95da
Ludwing08 <pansitas.2001@gmail.com>
2022-07-17 11:02:13 -0500

    PAD-46 <API-EDITAR-USUARIO FINALIZADA>

---
+++ frontend_arriendos/src/app/services/api-usuarios.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiUsuariosService {
+
+  private url = environment.url
+
+  constructor(private http : HttpClient) { }
+
+  listarUsuarios() : Observable<any>{
+    return this.http.get(`${this.url}/usuarios`)
+  }
+
+  editarUsuario(id: number, usuario: any): Observable<any>{
+    return this.http.put(`${this.url}/Usuarios/${id}`, usuario)
+  }
+
+  obtenerUsuarioPorId(id : number): Observable<any>{
+    return this.http.get(`${this.url}/usuarios/${id}`)
+  }
+
+}
